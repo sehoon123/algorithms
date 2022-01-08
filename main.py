f2e1ffdb74538762bf7af8c8172729461dfd901f
sehoon123 <sehoon0519@naver.com>
2022-01-09 03:08:03 +0900

    Changes

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 n = int(input())
 
 count = 0
+array = [500, 100, 50, 10]
 
-while n > 0:
-    if n - 500 >= 0:
-        n -= 500
-        count += 1
-    elif n - 100 >= 0:
-        n -= 100
-        count += 1
-    elif n - 50 >= 0:
-        n -= 50
-        count += 1
-    elif n - 10 >= 0:
-        n -= 50
-        count += 1
+for coin in array:
+    count += n // coin
+    n %= coin
+
+# while n > 0:
+#     if n - 500 >= 0:
+#         n -= 500
+#         count += 1
+#     elif n - 100 >= 0:
+#         n -= 100
+#         count += 1
+#     elif n - 50 >= 0:
+#         n -= 50
+#         count += 1
+#     elif n - 10 >= 0:
+#         n -= 50
+#         count += 1
 
 print(count)
-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
+# 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
