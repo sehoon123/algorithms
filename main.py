91b0d74a59f62462f932c0bc196a97e07c61e4aa
sehoon <sehoon0519@naver.com>
2022-01-09 02:30:20 +0900

    Python algorithms

---
+++ main.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+count = 0
+
+while n > 0:
+    if n - 500 >= 0:
+        n -= 500
+        count += 1
+    elif n - 100 >= 0:
+        n -= 100
+        count += 1
+    elif n - 50 >= 0:
+        n -= 50
+        count += 1
+    elif n - 10 >= 0:
+        n -= 50
+        count += 1
+
+print(count)
