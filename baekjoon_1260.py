77d334d292bbe2dbbe479fc042e013001b51277d
sehoon123 <sehoon0519@naver.com>
2022-01-12 00:41:48 +0900

    dfs and bfs

---
+++ baekjoon_1260.py
@@ -0,0 +1,66 @@
+import collections
+n,m,v = map(int, input().split())
+
+# edges = []
+# for _ in range(m):
+#     a = list(map(int, input().split()))
+#     a.sort()
+#     edges.append(a)
+#
+adj = [[] for _ in range(n+1)]
+for _ in range(m):
+    src, dest = map(int, input().split())
+    adj[src].append(dest)
+    adj[dest].append(src)
+
+for i in range(len(adj)):
+    adj[i].sort()
+
+
+print(adj)
+
+visited=[False]*(n+1)
+def dfs(adj, start, visited):
+    visited[start] = True
+    print(start, end=" ")
+    for node in adj[start]:
+        if not visited[node]:
+            dfs(adj, node, visited)
+
+dfs(adj, v, visited)
+
+
+# edges.sort()
+#
+# visited =[False]*(n+1)
+#
+# def dfs(edges, start, visited):
+#     visited[start] = True
+#     print(start, end = " ")
+#     for edge in edges:
+#         if start in edge:
+#             for v in edge:
+#                 if not visited[v]:
+#                     visited[v] = True
+#                     dfs(edges, v, visited)
+#
+# dfs(edges, v, visited)
+#
+# def bfs(edges, start, visited):
+#     visited[start] = True
+#     queue = collections.deque()
+#     queue.append(start)
+#     while queue:
+#         node = queue.popleft()
+#         print(node, end = " ")
+#         for edge in edges:
+#             if node in edge:
+#                 for v in edge:
+#                     if not visited[v]:
+#                         visited[v] = True
+#                         queue.append(v)
+#
+#
+# visited =[False]*(n+1)
+# print()
+# bfs(edges, v, visited)
