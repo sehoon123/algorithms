1a1ee93dbb139c0b347396ae614c2b57bb0598ef
sehoon123 <sehoon0519@naver.com>
2022-01-21 02:36:39 +0900

    DP 다시 한번 보기

--- baekjoon_12865.py
+++ baekjoon_12865.py
@@ -1,12 +1,21 @@
 n, k = map(int, input().split())
-products = []
-for _ in range(n):
+
+item = []
+for i in range(n):
     w, v = map(int, input().split())
-    products.append((w,v))
+    item.append((w, v))
 
-d = [0] * (k+1)
-for w, v in products:
-    for i in range(k, w-1, -1):
-        d[i] = max(d[i], d[i-w] + v)
+# print(item)
 
-print(d[-1])
+d = [[0] * (k+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(1, k+1):
+       if j >= item[i-1][0]:
+           d[i][j] = max(d[i-1][j], d[i-1][j-item[i-1][0]] + item[i-1][1])
+       else:
+           d[i][j] = d[i-1][j]
+
+
+for v in d:
+    print(v)
