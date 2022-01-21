d25c02cb2491197d0004a741c3118cc8ee869f82
sehoon123 <sehoon0519@naver.com>
2022-01-22 02:22:44 +0900

    knapsack recursive memoization

--- baekjoon_12865_1.py
+++ baekjoon_12865_1.py
@@ -1,24 +1,27 @@
 n, k = map(int, input().split())
 
-item = []
+w = [0 for _ in range(101)]
+v = [0 for _ in range(101)]
+
 
 for i in range(n):
-    w, v = map(int, input().split())
-    item.append((w, v))
+    a, b = map(int, input().split())
+    w[i] = a
+    v[i] = b
 
 d = [[0] * (101) for _ in range(100001)]
-def knapsack(i, w):
-    if d[i][w] > 0:
-        return d[i][w]
+def knapsack(a, b):
+    if d[a][b] > 0:
+        return d[a][b]
     if i == n:
         return 0
     n1 = 0
-    if w + item[i][0] <= k:
-        n1 = item[i][1] + knapsack(i+1, w + item[i][0])
-    n2 = knapsack(i+1, w)
+    if b + w[a] <= k:
+        n1 = w[a] + knapsack(i+1, b + w[a])
+    n2 = knapsack(a+1, b)
 
-    d[i][w] = max(n1, n2)
-    return d[i][w]
+    d[a][b] = max(n1, n2)
+    return d[a][b]
 
 print(knapsack(0,0))
 
