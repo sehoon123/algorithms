2510941d5287444263c19dc6fda6baaeab18db3e
sehoon123 <sehoon0519@naver.com>
2022-02-14 00:31:31 +0900

    using deepcopy

--- shortest path/test/1.py
+++ shortest path/test/1.py
@@ -1,2 +1,44 @@
-for i in range(10005):
-    print(i,end="")
+import copy
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+
+l, w = map(int, input().split())
+graph = []
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+for _ in range(l):
+    graph.append(list(input().strip()))
+
+
+def bfs():
+    global cnt
+    while q:
+        x, y = q.popleft()
+        temp[x][y] = 'W'
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < l and 0 <= ny < w and temp[nx][ny] == 'L':
+                q.append((nx, ny))
+                distance[nx][ny] = distance[x][y] + 1
+                cnt = max(cnt, distance[nx][ny])
+
+
+MAX = 0
+cnt = 0
+for i in range(l):
+    for j in range(w):
+        temp = copy.deepcopy(graph)
+        if temp[i][j] == 'L':
+            distance = [[0] * w for _ in range(l)]
+            q = deque()
+            q.append((i, j))
+            bfs()
+            MAX = max(MAX, cnt)
+            cnt = 0
+
+print(MAX)
