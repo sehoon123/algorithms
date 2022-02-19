2714cfaa07bf7f85bea601191dbd0180f1f38810
sehoon123 <sehoon0519@naver.com>
2022-02-19 22:24:18 +0900

    bfs

--- baekjoon_2573.py
+++ baekjoon_2573.py
@@ -1,33 +1,73 @@
 from collections import deque
-import sys
 import copy
+import sys
+
+
 input = sys.stdin.readline
+dx = [1, -1, 0, 0]
+dy = [0, 0, -1, 1]
 n, m = map(int, input().split())
 graph = []
-ice = deque()
-dx = [-1.0,0,1]
-dy = [0, 1, -1, 0]
-for i in range(n):
-    temp = list(input().split())
-    graph.append(temp)
-    for j in range(len(temp)):
-        if graph[i][j] != '0':
-            ice.append([i,j,temp[j]])
 
-tempGraph = copy.deepcopy(graph)
-def bfs():
-    while ice:
-        count = 0
-        x, y, z = ice.popleft()
+
+def bfs(x, y):
+    q = deque()
+    q.append([x, y])
+    visit = [[0] * m for i in range(n)]
+    visit[x][y] = 1
+    while q:
+        x, y = q.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m:
-                if graph[nx][ny] == '0':
-                    count += 1
+            if 0 <= nx < n and 0 <= ny < m and visit[nx][ny] == 0 and temp[nx][ny] != 0:
+                temp[nx][ny] = 0
+                visit[nx][ny] = 1
+                q.append([nx, ny])
 
 
-                    
+def check(x, y):
+    cnt = 0
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < n and 0 <= ny < m and t[nx][ny] == 0:
+            cnt += 1
+    return cnt
 
 
+def checkz():
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] != 0:
+                return False
+    return True
 
+
+for i in range(n):
+    graph.append(list(map(int, input().split())))
+result = 1
+
+
+while True:
+    if checkz():
+        print(0)
+        break
+    t = copy.deepcopy(graph)
+    for i in range(n):
+        for j in range(m):
+            if t[i][j] != 0:
+                temp = graph[i][j] - check(i, j)
+                graph[i][j] = temp if temp >= 0 else 0
+    temp = copy.deepcopy(graph)
+    cnt = 0
+    for i in range(n):
+        for j in range(m):
+            if temp[i][j] != 0:
+                temp[i][j] = 0
+                bfs(i, j)
+                cnt += 1
+    if cnt > 1:
+        print(result)
+        break
+    result += 1
